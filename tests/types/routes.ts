09bcf4ba89a67fa342e32fd5e1abd400d1606358
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-17 15:03:08 +0300

    chore: rm husky

--- tests/types/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { ElysiaWithBaseUrl } from "elysia-autoload";
-import type Route0 from "../routes/index";
-import type Route1 from "../routes/users/[id]";
-
-declare global {
-    export type Routes = ElysiaWithBaseUrl<"/", ReturnType<typeof Route0>>
-              & ElysiaWithBaseUrl<"/users/:id", ReturnType<typeof Route1>>
-}
