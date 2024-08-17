3b453417333d8d28c0b36185624a4581f90d04fd
deadlinecode <110681830+deadlinecode@users.noreply.github.com>
2024-08-17 23:32:24 +0200

    Flatten index routes

--- src/types.ts
+++ src/types.ts
@@ -10,30 +10,55 @@ type PathToObject<
     : { [K in Head]: PathToObject<Rest, Type> }
   : { [K in Path]: Type };
 
+type RouteEndType = Record<
+  string,
+  {
+    body: any;
+    params: any;
+    query: any;
+    headers: any;
+    response: any;
+  }
+>;
+
+type FlattenIndexRoutes<T> = T extends object
+  ? {
+      [K in keyof T as K extends "index"
+        ? T[K] extends RouteEndType
+          ? never
+          : K
+        : K]: FlattenIndex<T[K]>;
+    } & (T extends { index: infer I }
+      ? I extends RouteEndType
+        ? FlattenIndex<I>
+        : {}
+      : {})
+  : T;
+
 export type ElysiaWithBaseUrl<
-	BaseUrl extends string,
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	ElysiaType extends Elysia<any, any, any, any, any, any, any, any>,
+  BaseUrl extends string,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  ElysiaType extends Elysia<any, any, any, any, any, any, any, any>
 > = ElysiaType extends Elysia<
-	infer BasePath,
-	infer Scoped,
-	infer Singleton,
-	infer Definitions,
-	infer Metadata,
-	infer Routes,
-	infer Ephemeral,
-	infer Volatile
+  infer BasePath,
+  infer Scoped,
+  infer Singleton,
+  infer Definitions,
+  infer Metadata,
+  infer Routes,
+  infer Ephemeral,
+  infer Volatile
 >
-	? Elysia<
-			BasePath,
-			Scoped,
-			Singleton,
-			Definitions,
-			Metadata,
-			PathToObject<BaseUrl, Routes>,
-			Ephemeral,
-			Volatile
-		>
-	: never;
+  ? Elysia<
+      BasePath,
+      Scoped,
+      Singleton,
+      Definitions,
+      Metadata,
+      FlattenIndexRoutes<PathToObject<BaseUrl, Routes>>,
+      Ephemeral,
+      Volatile
+    >
+  : never;
 
 export type SoftString<T extends string> = T | (string & {});
