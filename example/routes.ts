249f39889fbc411eba7b6d908d69eb8111d518cb
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-08-11 23:19:43 +0300

    chore: update example and bump

--- example/routes.ts
+++ example/routes.ts
@@ -1,9 +1,9 @@
 import type { ElysiaWithBaseUrl } from "elysia-autoload";
-import type Route0 from "./routes/index";
-import type Route1 from "./routes/exports/default";
-import type Route2 from "./routes/exports/elysia";
-import type Route3 from "./routes/exports/zero-aguments";
-import type Route4 from "./routes/test/[some]/index";
+import type Route0 from "./routes/index.ts";
+import type Route1 from "./routes/exports/default.ts";
+import type Route2 from "./routes/exports/elysia.ts";
+import type Route3 from "./routes/exports/zero-aguments.ts";
+import type Route4 from "./routes/test/[some]/index.ts";
 
 declare global {
     export type Routes = ElysiaWithBaseUrl<"/api", typeof Route0>
